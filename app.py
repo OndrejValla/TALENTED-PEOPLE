3f9ad6832b2ad92344da69a6647eabd3eb96dc5a
OndrejValla <ondrej.valla@gmail.com>
2021-08-29 22:48:09 +0000

    Add Register Defensive Programming

--- app.py
+++ app.py
@@ -17,22 +17,40 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+# HOME PAGE FUNCTION
 @app.route("/")
 @app.route("/get_home")
 def get_home():
     return render_template("home.html")
 
-
+# GET PHOTOGRAPHERS FUNCTION
 @app.route("/")
 @app.route("/get_photographers")
 def get_photographers():
     photographers = mongo.db.photographers.find()
     return render_template("photographers.html", photographers=photographers)
 
-
+# REGISTER FUNCTION
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username is already registred in our database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists! Try different one.")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into "session" cookie
+        session["user"] = request.form.get("username").lower()
+        flash("You have been successfully registred!")
     return render_template("register.html")
 
 
