2bffb247a3ff32bc1bbee87e0023bef62d067d5a
OndrejValla <ondrej.valla@gmail.com>
2021-09-08 13:59:23 +0000

    Add categories.html

--- app.py
+++ app.py
@@ -209,6 +209,13 @@ def profile(username):
         return redirect(url_for("login", ))
 
 
+# MANAGE CATEGORIES FUNCTION
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
